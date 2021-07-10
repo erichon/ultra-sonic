9ca05e911dc4145b610c5175e385b2805b617adc
erichon <erichon18@gmail.com>
2021-07-10 18:10:18 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+let distance = 0
+basic.forever(function () {
+    distance = sonar.ping(
+    DigitalPin.P0,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    )
+    basic.showNumber(distance)
+    basic.pause(1000)
+})
